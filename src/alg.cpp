763bcc7de4ce18267a136191ac9c9159d928f04b
deniskr12 <kremnden@gmail.com>
2024-02-26 22:14:27 +0300

    ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,13 @@
 #include <cstdint>
 #include "alg.h"
 
+
 bool checkPrime(uint64_t value) {
-    for (int i = 2; i <= (value/2); i++) {
-        if (value == 2)
+    for (uint64_t i = 2; i <= (value / 2); i++) {
+        if (value == 2) {
             return true;
-        if ((value % i) == 0) {
+        }
+        if (value % i == 0) {
             return false;
         }
     }
@@ -14,29 +16,34 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    int count = 0;
-    for (int i = 2; i <= 10000; i++) {
-        if (checkPrime(i) == true)
-            ++count;
-        if (count == n)
+    uint64_t iCount = 0;
+    for (uint64_t i = 2; i < 1000000; i++) {
+        if (checkPrime(i)) {
+            ++iCount;
+        }
+        if (iCount == n) {
             return i;
+        }
     }
     return 0;
 }
 
+
 uint64_t nextPrime(uint64_t value) {
-    for (++value; value <= 10000000; value++) {
-        if (checkPrime(value) == true)
+    while (true) {
+        value++;
+        if (checkPrime(value) == true) {
             return value;
+        }
     }
-    return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
+    uint64_t iBound = 0;
     for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i) == true)
-            sum += i;
+        if (checkPrime(i) == true) {
+            iBound += i;
+        }
     }
-    return sum;
+    return iBound;
 }
